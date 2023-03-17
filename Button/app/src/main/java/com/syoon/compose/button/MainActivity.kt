cc1883b867e47e028702e39221a5d213a6e2608f
seoyoon513 <syoonlee513@gmail.com>
2023-03-17 16:19:38 +0900

    [chore] #2 버튼 예제 기본 세팅

--- Button/app/src/main/java/com/syoon/compose/button/MainActivity.kt
+++ Button/app/src/main/java/com/syoon/compose/button/MainActivity.kt
@@ -3,12 +3,9 @@ package com.syoon.compose.button
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.syoon.compose.button.ui.theme.ButtonTheme
 
@@ -17,27 +14,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ButtonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ButtonExample(onButtonClicked = {})
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ButtonExample(onButtonClicked: () -> Unit) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Send")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ButtonTheme {
-        Greeting("Android")
+        ButtonExample(onButtonClicked = {})
     }
 }
