a6c8045cb4cc936258fdeb490d7bfebae39acc89
seoyoon513 <syoonlee513@gmail.com>
2023-04-02 16:19:56 +0900

    [feat] #9 정적인 이미지 가져오기

--- Image/app/src/main/java/com/syoon/compose/image/MainActivity.kt
+++ Image/app/src/main/java/com/syoon/compose/image/MainActivity.kt
@@ -3,12 +3,12 @@ package com.syoon.compose.image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.syoon.compose.image.ui.theme.ImageTheme
 
@@ -17,27 +17,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ImageTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ImageEx()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ImageEx() {
+    Column {
+        // Step1 : Image 만들기
+        Image(
+            painter = painterResource(id = R.drawable.wall),
+            contentDescription = "엔텔로프 캐넌"
+        )
+
+        // Step2 : ImageVector에 Incons.Filled.Settings를 설정
+        Image(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = "세팅"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ImageTheme {
-        Greeting("Android")
+        ImageEx()
     }
 }
