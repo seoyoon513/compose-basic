a1b3434ab9a6f81bed181fa2253b4a0bd03efc90
seoyoon513 <syoonlee513@gmail.com>
2023-04-02 16:57:43 +0900

    [feat] #9 network image 가져오기

--- Image/app/src/main/java/com/syoon/compose/image/MainActivity.kt
+++ Image/app/src/main/java/com/syoon/compose/image/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import com.syoon.compose.image.ui.theme.ImageTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ImageTheme {
-                ImageEx()
+                //ImageEx()
+                CoilEx()
             }
         }
     }
@@ -40,10 +43,33 @@ fun ImageEx() {
     }
 }
 
+@Composable
+fun CoilEx() {
+    Column {
+        // Step1 : rememberImagePainter를 이용하여 Image의 painter 설정
+        // deprecated 됐지만 Compose 한국어 문서에서는 추천
+        val painter =
+            rememberImagePainter(data = "https://user-images.githubusercontent.com/110798031/229339695-86b240e9-039f-47b8-8ae9-05f514de7311.png")
+
+        Image(
+            painter = painter,
+            contentDescription = "벌새"
+        )
+
+        // Step2 : AsyncImage 이용하여 image 띄우기
+        // 공식 라이브러리에서 추천하는 방법
+        AsyncImage(
+            model = "https://user-images.githubusercontent.com/110798031/229339695-86b240e9-039f-47b8-8ae9-05f514de7311.png",
+            contentDescription = "벌새"
+        )
+    }
+}
+
+// 네트워크를 통해 가져오는 이미지는 preview로 볼 수 없음
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ImageTheme {
-        ImageEx()
+        //ImageEx()
     }
 }
