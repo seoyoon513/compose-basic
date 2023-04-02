e9ee7dbb0ca108e3603a0b9e5d2daa1df104493c
seoyoon513 <syoonlee513@gmail.com>
2023-04-02 11:23:27 +0900

    [feat] #4 Surface 테스트

--- Surface/app/src/main/java/com/syoon/compose/surface/MainActivity.kt
+++ Surface/app/src/main/java/com/syoon/compose/surface/MainActivity.kt
@@ -3,13 +3,20 @@ package com.syoon.compose.surface
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.syoon.compose.surface.ui.theme.SurfaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +38,80 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    Column() {
+        Surface(
+            color = MaterialTheme.colorScheme.surface,
+            modifier = Modifier
+                .padding(5.dp)
+        ) {
+            Text(
+                text = "Hello $name!", modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+        }
 
+        // Step1 : Surfae에 elevation 설정
+        Surface(
+            shadowElevation = 5.dp, // M2에서 elevation
+            modifier = Modifier.padding(5.dp)
+        ) {
+            Text(
+                text = "Hello $name!", modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+        }
+
+        // Step2 : border 값 설정
+        Surface(
+            border = BorderStroke(
+                width = 2.dp,
+                color = Color.Magenta
+            ),
+            modifier = Modifier.padding(5.dp)
+        ) {
+            Text(
+                text = "Hello $name!", modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+        }
+
+        // Step3 : Surface의 shape 설정
+        Surface(
+            border = BorderStroke(
+                width = 2.dp,
+                color = Color.Magenta
+            ),
+            modifier = Modifier.padding(5.dp),
+            shape = CircleShape
+        ) {
+            Text(
+                text = "Hello $name!", modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+        }
+
+        // Step4 : color 지정
+        Surface(
+            border = BorderStroke(
+                width = 2.dp,
+                color = Color.Magenta
+            ),
+            modifier = Modifier.padding(5.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colorScheme.secondary,
+            //contentColor = Color.Black
+        ) {
+            Text(
+                text = "Hello $name!", modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
