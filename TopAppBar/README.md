e9af304acb257c1653973027f9655653a224e0c4
seoyoon513 <syoonlee513@gmail.com>
2023-11-19 21:43:55 +0900

    [feat] #13 TopAppBar 테스트

---
+++ TopAppBar/README.md
@@ -0,0 +1,2 @@
+## TopAppBar 연습
+![image](https://github.com/seoyoon513/compose-basic/assets/110798031/fedb0d5a-2a48-4afe-aba2-d89dd30a8c60)
--- TopAppBar/app/src/main/java/com/syoon/compose/topappbar/MainActivity.kt
+++ TopAppBar/app/src/main/java/com/syoon/compose/topappbar/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.syoon.compose.topappbar
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.syoon.compose.topappbar.ui.theme.TopAppBarTheme
@@ -22,25 +35,85 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column {
+        TopAppBar(
+            title = { Text(text = "TopAppBar") },
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "업 네비게이션"
+                    )
+                }
+            }, actions = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "검색"
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "설정"
+                    )
+                }
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.AccountBox,
+                        contentDescription = "계정"
+                    )
+                }
+            }
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "업 네비게이션"
+                )
+            }
+            /*
+            weight를 하나만 지정할 경우 남은 여백을 다 채운다
+             */
+            Text(text = "TopAppBarTheme", modifier = Modifier.weight(1f))
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "검색"
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "설정"
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.AccountBox,
+                    contentDescription = "계정"
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TopAppBarTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
